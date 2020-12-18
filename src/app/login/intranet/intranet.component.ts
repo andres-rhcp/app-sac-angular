81ba167f6b41b8dcc3ad44ca3edd0f1090c55c5b
andres-rhcp <andres-rhcp@hotmail.com>
2020-12-18 14:06:58 -0500

    if result == ok

--- src/app/login/intranet/intranet.component.ts
+++ src/app/login/intranet/intranet.component.ts
@@ -3,14 +3,14 @@ import { LoadingService, ServidorService, SnackBarService, ConstantsService, Sha
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogComponent } from '../../_components/dialog/dialog.component';
 import { map, first } from 'rxjs/operators';
-import {CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot} from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 @Component({
   selector: 'app-intranet',
   templateUrl: './intranet.component.html',
   styleUrls: ['./intranet.component.css']
 })
 export class IntranetComponent implements OnInit {
-  urlFielWeb:string;
+  urlFielWeb: string;
   constructor(private loadingService: LoadingService,
     private dialog: MatDialog,
     private servidorService: ServidorService,
@@ -29,21 +29,22 @@ export class IntranetComponent implements OnInit {
       data: { title: 'Confirmación', message: '¿Está seguro de redirigirse a Fiel Web?' },
       panelClass: 'my-class'
     });
-    
+
     dialogRef.afterClosed().subscribe(result => {
-      // this.loadingService.show();
-      this.servidorService.abrirFieldWeb(this.constantsService.token)
-      .pipe(first())
-      .subscribe(
-        respuesta => { 
-          
-          this.urlFielWeb=this.constantsService.urlFielWeb+respuesta.token.toString().split('<ObtenerTokenResult>')[1].toString().split('</ObtenerTokenResult>')[0].toString();
-          // window.location.href = this.router.navigate[this.urlFielWeb];
-          window.open(this.urlFielWeb, '_blank');
-          console.log(this.urlFielWeb)
-        });
+      this.loadingService.show();
+      if (result == 'OK') {
+        this.servidorService.abrirFieldWeb(this.constantsService.token)
+          .pipe(first())
+          .subscribe(
+            respuesta => {
+
+              this.urlFielWeb = this.constantsService.urlFielWeb + respuesta.token.toString().split('<ObtenerTokenResult>')[1].toString().split('</ObtenerTokenResult>')[0].toString();
+              // window.location.href = this.router.navigate[this.urlFielWeb];
+              window.open(this.urlFielWeb, '_blank');
+              console.log(this.urlFielWeb)
+            });
+      }
       this.loadingService.hide();
     });
   }
-
 }
