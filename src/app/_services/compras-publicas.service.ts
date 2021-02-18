94ad3f16fedbc7c4051967691dc3fa31068ec104
carogab99 <59735264+carogab99@users.noreply.github.com>
2021-02-18 12:59:26 -0500

    cambios varios
    modificacion punto de informacion y creacion de login compras publicas

--- src/app/_services/compras-publicas.service.ts
+++ src/app/_services/compras-publicas.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComprasPublicasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postIniciarSesionCP(cedula, pasw){
+    return this.http.post('http://localhost:3000/login/loginMunicipio',{cedula, pasw});
+  }
+
 }
--- src/app/compras-publicas/login-cp/login-cp.component.ts
+++ src/app/compras-publicas/login-cp/login-cp.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
+import {Md5} from "md5-typescript";
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from 'src/app/_models';
+import { ComprasPublicasService } from 'src/app/_services/compras-publicas.service';
 
 @Component({
   selector: 'app-login-cp',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginCpComponent implements OnInit {
 
-  constructor() { }
+  datos: any = []
+  cedula: string;
+  password: string;
+
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+  public usuario: string;
+
+  constructor(private ComprasPublicasService: ComprasPublicasService, 
+    private router: Router, 
+    public dialog: MatDialog) 
+    {
+      this.currentUserSubject = new BehaviorSubject<any>((localStorage.getItem('usuario')));
+      this.currentUser = this.currentUserSubject.asObservable();
+      this.usuario = (localStorage.getItem('usuario'));
+    }
 
   ngOnInit() {
   }
+
+  loginComprasPublicas(){
+    const pswd = Md5.init(this.password)
+    this.ComprasPublicasService.postIniciarSesionCP(this.cedula, pswd).subscribe(
+      data =>{
+        this.datos = data
+        if(this.datos.mensaje ==='OK-Usua-Muni'){
+          localStorage.setItem('cedula', this.datos.cedula);
+          localStorage.setItem('nombres', this.datos.nombres);
+          localStorage.setItem('apellidos', this.datos.apellidos);
+          localStorage.setItem('departamento', this.datos.departamento);
+          localStorage.setItem('jefe', this.datos.jefe);
+          this.currentUserSubject.next(this.datos);
+
+          this.router.navigate(['compras-publicas/menu'])
+        }else{
+
+        }
+      }
+    )
+  }
+  
 
 }
